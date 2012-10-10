b0a165ff6b909a566fa0a71256613c7f609d859b
johnlyle <john.lyle@cs.ox.ac.uk>
2012-10-10 21:47:31 +0100

    Modified the display to show maps of where reminders are set

--- main.js
+++ main.js
@@ -31,6 +31,8 @@ function loadApp() {
 	makeEnabled();
 	$("#viewReminders").click(function() {
 		showRemindersPage();
+		showPlaces();
+		$("#frontpage").append("<div id='map'></div>");
 	});
 }
 
@@ -50,6 +52,13 @@ function showRemindersPage() {
 	$("#frontpage").append(table);
 }	
 
+function highlightPlace() {
+	var id = $(this).attr("id");
+	id = id.substring(id.indexOf("-")+1, id.length);
+	console.log( id );	
+	showMapId(id);
+
+}
 
 function showReminderRow(reminder) {
 	console.log("Displaying " + JSON.stringify(reminder));
@@ -58,21 +67,83 @@ function showReminderRow(reminder) {
 	var desc  = $('<td></td>').text(reminder.description);
 	var when  = $('<td></td>');	
 	for (var i=0;i<when.length;i++) {
-		when.append(reminder.when[i].date + ", Recurrance: " + reminder.when[i].recurring + "<br />");
+		when.append(reminder.when[i].date + ", Recurrance: " + 
+			reminder.when[i].recurring + "<br />");
 	}
 	var where = $('<td></td>')	
 	for (var i=0;i<where.length;i++) {
-
-		where.append("<a href=''>" + reminder.where[i].place.description + 
-			"</a> (within " + reminder.where[i].proximity.amount + " " +  
-			reminder.where[i].proximity.units + ") <br/>");
-//		where.text(JSON.stringify(reminder.where[i].plac));
+		where.append(createWhereLink(reminder.where[i]));
+		where.append(" (within " + reminder.where[i].proximity.amount + " " +  
+		reminder.where[i].proximity.units + ") <br/>");
 	}
 	
 	row.append(desc);
 	row.append(when); 
 	row.append(where);
 	return row;
+}
+
+
+function createWhereLink(whereObject) {
+	var whereLink = $('<a></a>');
+	whereLink.attr('id', "linkto-" + whereObject.place.id);
+	whereLink.attr('href', "#" + whereObject.place.id);
+	whereLink.addClass("whereLink");
+	whereLink.click(highlightPlace);
+	whereLink.text(whereObject.place.description);
+	return whereLink;
+}
+
+function showPlaces() {
+
+	var list = $('<ul></ul>');
+	list.addClass('placeList');
+
+    for (var i=0; i<places.length; i++) {
+		var placeItem = $('<li></li>');
+		var placeLink = $('<a></a>');
+		placeLink.text(places[i].description);
+		placeLink.attr('id', "" + places[i].id);
+		placeLink.attr('href', "#" + places[i].id);
+		placeLink.click(showMap);
+		placeItem.append(placeLink);
+		list.append(placeItem);
+	}	
+	var heading = $('<h1></h1>').text("Places");
+
+
+	$("#frontpage").append(heading);
+	$("#frontpage").append(list);
+
+}
+
+function showMapId(id) {
+	for (var i=0;i<places.length;i++) {		
+		if (places[i].id === id) {
+			console.log("Found the place: " + places[i].id + " - " + places[i].description);			
+			$('#map').html(getGoogleMap(places[i].coordinates));
+			$('#' + id).addClass("selectedPlace");
+		} else {
+			console.log("Not the place: " + places[i].id + " - " + places[i].description);
+			$('#' + id).removeClass("selectedPlace");
+		}
+	}
+}
+
+function showMap() {
+	var id = $(this).attr("id");
+	showMapId(id);
+}
+
+function getGoogleMap(coords) {
+	var image_url = "http://maps.googleapis.com/maps/api/staticmap?" + 
+			"center=" + 
+			coords.latitude + "," +
+            coords.longitude + 
+			"&zoom=14&size=400x400&sensor=false"
+	var gMap = $("<img />");
+	gMap.attr("src", image_url).attr('id','imgmap');
+	return gMap;
 }
 
 
