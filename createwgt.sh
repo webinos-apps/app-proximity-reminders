3db650b34bd9dc7fc8acc46278e0e141b4554af6
johnlyle <john.lyle@cs.ox.ac.uk>
2012-10-22 17:13:57 +0300

    The createwgt script now creates two versions: one with and one without webinos.js

--- createwgt.sh
+++ createwgt.sh
@@ -5,3 +5,9 @@ if [ -e ./proximityreminder.wgt ]; then
 fi
 
 zip proximityreminder.wgt *.html ./js/*.js ./css/*.css ./images/* config.xml remind_me.png
+
+if [ -e ./proximityreminder-nowebinosjs.wgt ]; then 
+    rm -v proximityreminder-nowebinosjs.wgt
+fi
+
+zip proximityreminder-nowebinosjs.wgt *.html ./js/*.js ./css/*.css ./images/* config.xml remind_me.png -x ./js/webinos.js
